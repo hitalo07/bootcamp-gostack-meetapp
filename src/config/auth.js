cace55c2c71bb58f35848b264c670c1a8194f5ef
hitalo07 <hitalocunhadesousa@gmail.com>
2019-08-26 08:42:17 -0300

    Initial Commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ceaf4a8cdc2665465aa194764d9842a3',
+  expiresIn: '7d',
+}
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'meetup',
+  password: 'meetup',
+  database: 'meetup',
+  define: {
+    timestamps: true,
+    underscored: true,
+    undescoredAll: true,
+  },
+}
