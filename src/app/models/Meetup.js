b3b391a15f38baf0557f905624a0623d1bb47763
hitalo07 <hitalocunhadesousa@gmail.com>
2019-09-06 01:35:33 -0300

    feat: Files

---
+++ src/app/models/Meetup.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize'
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.STRING,
+        location: Sequelize.STRING,
+        date: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    )
+
+    return this
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id' })
+    this.belongsTo(models.File, { foreignKey: 'file_id' })
+  }
+}
+
+export default Meetup
